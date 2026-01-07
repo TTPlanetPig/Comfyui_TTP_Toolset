4b23cd5cf5da487eb3c710128b2b8ca1f2b8974e
TTPlanetPig <152850462+TTPlanetPig@users.noreply.github.com>
2026-01-07 23:50:54 +0800

    update for LTXV first to last I2V control support

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
-from .TTP_toolsets import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .TTP_toolsets import NODE_CLASS_MAPPINGS as TOOLSET_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as TOOLSET_DISPLAY
+from .LTXVFirstLastFrameControl_TTP import LTXVFirstLastFrameControl_TTP
+
+NODE_CLASS_MAPPINGS = {
+    **TOOLSET_MAPPINGS,
+    "LTXVFirstLastFrameControl_TTP": LTXVFirstLastFrameControl_TTP,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    **TOOLSET_DISPLAY,
+    "LTXVFirstLastFrameControl_TTP": "LTX First Last Frame Control (TTP)",
+}
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
